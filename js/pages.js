6706fdb98512107a869ed6bdd8a7ff33359a2684
Winner_don <olusolawinner1@gmail.com>
2022-05-24 19:28:41 +0100

    update

---
+++ js/pages.js
@@ -0,0 +1,27 @@
+function demoall() {
+    document.getElementById("pageall").style.display = "block";
+    document.getElementById("pagesoccer").style.display= "none";
+    document.getElementById("pagebasket").style.display = "none";
+    document.getElementById("pagecricket").style.display = "none";
+  }
+  
+function demosoccer() {
+    document.getElementById("pageall").style.display = "none";
+    document.getElementById("pagesoccer").style.display = "block";
+    document.getElementById("pagebasket").style.display = "none";
+    document.getElementById("pagecricket").style.display = "none";
+  }
+
+function demobasket() {
+    document.getElementById("pageall").style.display = "none";
+    document.getElementById("pagesoccer").style.display= "none";
+    document.getElementById("pagebasket").style.display = "block";
+    document.getElementById("pagecricket").style.display = "none";
+  }
+  
+function democricket() {
+    document.getElementById("pageall").style.display = "none";
+    document.getElementById("pagesoccer").style.display = "none";
+    document.getElementById("pagebasket").style.display = "none";
+    document.getElementById("pagecricket").style.display = "block";
+  }
