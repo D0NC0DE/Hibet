58f73c0a3735b7ea162ebf3eb8a2bd7015000c8c
Winner_don <olusolawinner1@gmail.com>
2022-05-24 17:22:51 +0100

    interface

--- js/connect.js
+++ js/connect.js
@@ -11,7 +11,7 @@ var btn2 = document.getElementById("connect");
 
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close");
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
